127b6f5216dc2c65eab975304fbb763be8ecca91
awashbrook <andy.washbrook@thomsonreuters.com>
2021-08-16 09:48:02 +0100

    Terraform format...

--- root.tf
+++ root.tf
@@ -1,8 +1,8 @@
 module "compartments" {
   source       = "./compartments"
-  tenancy_ocid = "${var.tenancy_ocid}"
-  app_tag      = "${var.app_tag}"
-  environment  = "${var.environment}"
+  tenancy_ocid = var.tenancy_ocid
+  app_tag      = var.app_tag
+  environment  = var.environment
 
   providers = {
     oci = oci.home
@@ -10,22 +10,22 @@ module "compartments" {
 }
 
 module "a_vcn" {
-  source           = "./vcn"
-  tenancy_ocid     = "${var.tenancy_ocid}"
-  compartment_ocid = "${module.compartments.networks_id}"
-  application_compartment_ocid = "${module.compartments.application_id}"
-  bastion_compartment_ocid = "${module.compartments.bastion_id}"
+  source                       = "./vcn"
+  tenancy_ocid                 = var.tenancy_ocid
+  compartment_ocid             = module.compartments.networks_id
+  application_compartment_ocid = module.compartments.application_id
+  bastion_compartment_ocid     = module.compartments.bastion_id
 
-  app_tag          = "${var.app_tag}"
-  environment      = "${var.environment}"
-  vcn_cidr         = "${var.vcn_cidr}"
+  app_tag     = var.app_tag
+  environment = var.environment
+  vcn_cidr    = var.vcn_cidr
 }
 
 module "iam" {
   source       = "./iam"
-  tenancy_ocid = "${var.tenancy_ocid}"
-  app_tag      = "${var.app_tag}"
-  environment  = "${var.environment}"
+  tenancy_ocid = var.tenancy_ocid
+  app_tag      = var.app_tag
+  environment  = var.environment
 
   providers = {
     oci = oci.home
--- variables.tf
+++ variables.tf
@@ -7,16 +7,16 @@ variable "image_id" {
   default = {
     // See https://docs.cloud.oracle.com/iaas/images/ 
     // Oracle-provided image "Oracle-Linux-7.4-2018.02.21-1"
-    us-phoenix-1 = "ocid1.image.oc1.phx.aaaaaaaaapqrkiqsuj2yxoypb26z6k4wt47fhynnr36zyhdn6to26fd4l54q"
-    us-ashburn-1 = "ocid1.image.oc1.iad.aaaaaaaajlpkjb5lpw36fx2nsxmw7cxle2r5ox7wtj3wcgnvviih6p725jba"
+    us-phoenix-1   = "ocid1.image.oc1.phx.aaaaaaaaapqrkiqsuj2yxoypb26z6k4wt47fhynnr36zyhdn6to26fd4l54q"
+    us-ashburn-1   = "ocid1.image.oc1.iad.aaaaaaaajlpkjb5lpw36fx2nsxmw7cxle2r5ox7wtj3wcgnvviih6p725jba"
     eu-frankfurt-1 = "ocid1.image.oc1.eu-frankfurt-1.aaaaaaaayaoazkfukf2ug7adabq5nb2dmyu45s6zpwwky7pd5aegqepj45ba"
-    uk-london-1 = "ocid1.image.oc1.uk-london-1.aaaaaaaak2aelt2ihexd23lcfwq6fert3nbufv3tphgmpxeczdwmrgpcklga"
+    uk-london-1    = "ocid1.image.oc1.uk-london-1.aaaaaaaak2aelt2ihexd23lcfwq6fert3nbufv3tphgmpxeczdwmrgpcklga"
   }
 }
 variable "private_key_path" {}
 variable "private_key_password" {}
 variable "instance_username" {
- default = "opc"
+  default = "opc"
 }
 variable "ssh_private_key_path" {}
 variable "ssh_public_key_path" {}
