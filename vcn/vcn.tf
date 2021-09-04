360e80ea5517a5f0f477d9022b81ec074753922d
awashbrook <andy.washbrook@thomsonreuters.com>
2021-09-04 14:37:50 +0100

    Naming base makes less sense now vcn module re-used many times

--- vcn/vcn.tf
+++ vcn/vcn.tf
@@ -1,12 +1,12 @@
-resource "oci_core_virtual_network" "base_vcn" {
+resource "oci_core_virtual_network" "vcn" {
   cidr_block     = var.vcn_cidr
   compartment_id = var.network_compartment_id
   dns_label      = lower(format("%s", var.app_tag))
   display_name   = "${var.app_tag}_${var.environment}_vcn"
 }
 
-resource "oci_core_internet_gateway" "base_ig" {
-  vcn_id         = oci_core_virtual_network.base_vcn.id
+resource "oci_core_internet_gateway" "vcn_ig" {
+  vcn_id         = oci_core_virtual_network.vcn.id
   compartment_id = var.network_compartment_id
   display_name   = "${var.app_tag}_${var.environment}_internetgateway"
 }
--- vcn/vcn_output.tf
+++ vcn/vcn_output.tf
@@ -1,9 +1,6 @@
 output "vcn_id" {
-  value = oci_core_virtual_network.base_vcn.id
-}
-output "default_dhcp_id" {
-  value = oci_core_virtual_network.base_vcn.default_dhcp_options_id
+  value = oci_core_virtual_network.vcn.id
 }
 output "internet_gateway_id" {
-  value = oci_core_internet_gateway.base_ig.id
+  value = oci_core_internet_gateway.vcn_ig.id
 }
