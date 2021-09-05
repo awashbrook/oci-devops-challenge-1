59853485f7091bd762ab31f0a890c9deee6779ed
awashbrook <awashbrook@gmail.com>
2021-09-05 17:11:57 +0100

    Augment default VCN security list allow ingress from Bastion

--- vcn/variables.tf
+++ vcn/variables.tf
@@ -3,3 +3,22 @@ variable "vcn_cidr" {}
 variable "network_compartment_id" {}
 variable "app_tag" {}
 variable "environment" {}
+variable "bastion_vcn_cidr" {
+  default = "172.168.0.0/16"
+}
+
+variable "icmp_protocol" {
+  default = 1
+}
+variable "tcp_protocol" {
+  default = 6
+}
+variable "all_protocols" {
+  default = "all"
+}
+variable "anywhere" {
+  default = "0.0.0.0/0"
+}
+variable "ssh_port" {
+  default = "22"
+}
--- vcn/vcn.tf
+++ vcn/vcn.tf
@@ -16,3 +16,22 @@ resource "oci_core_local_peering_gateway" "vcn_lpg" {
   compartment_id = var.network_compartment_id
   display_name   = "${var.app_tag}_${var.environment}_localpeeringgateway"
 }
+
+resource "oci_core_default_security_list" "peering-vcns" {
+  manage_default_resource_id = oci_core_virtual_network.vcn.default_security_list_id
+  # TODO Kill this if this isnt default egress rules
+  egress_security_rules {
+    protocol    = var.all_protocols
+    destination = var.anywhere
+  }
+  # Allow ssh ingress from Bastion
+  ingress_security_rules {
+    protocol    = var.tcp_protocol
+    description = "Bastion admin ingress from public internet into application compartment"
+    source      = var.bastion_vcn_cidr
+    tcp_options {
+      min = var.ssh_port
+      max = var.ssh_port
+    }
+  }
+}
